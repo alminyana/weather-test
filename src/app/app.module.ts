18bb70257fdd146b3c0f65eea3b57bd05e23ffcb
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-08 11:10:23 +0200

    created header and model for city entity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CitiesListComponent } from "./cities-list/cities-list.component";
+import { CitiesListComponent } from "./cities/cities-list/cities-list.component";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CitiesComponent,
-    CitiesListComponent
+    CitiesListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
