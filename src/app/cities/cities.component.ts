61d975383193afde2517b4b9a149389c48fd0c8a
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-11 16:54:45 +0200

    call service from city-list component

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,28 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { CitiesService } from './service/cities.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
   styleUrls: ['./cities.component.css']
 })
-export class CitiesComponent implements OnInit {
+export class CitiesComponent {
 
-  constructor(private city: CitiesService) { }
-
-  ngOnInit() {
-    // this.city.getCityTemp()
-    //   .subscribe(
-    //     (data) => {
-    //       console.log('data', data);
-    //     },
-    //     (error) => {
-    //       console.log('error', error);
-    //     }
-    //   );
-
-    this.city.getCityTemp();
-
-  }
+  constructor() { }
 
 }
