e084314f9b5a498dcc1e5c433f7319d75cfdefaa
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-12 12:25:24 +0200

    set constant variables to readonly

--- src/app/cities/app-config.ts
+++ src/app/cities/app-config.ts
@@ -1,10 +1,12 @@
 export class AppConfig {
 
-  public static key: string = "&appid=4bf0c0449365422e36a0582c746b24d6";
+  static readonly key: string = '&appid=4bf0c0449365422e36a0582c746b24d6';
 
-  public static singleCityUrl: string = "http://api.openweathermap.org/data/2.5/weather?id=";
+  static readonly singleCityUrl: string = 'http://api.openweathermap.org/data/2.5/weather?id=';
 
-  public static cities: {name: string, id: number}[] = [
+  static readonly groupCityUrl: string = 'http://api.openweathermap.org/data/2.5/group?id=';
+
+  static readonly cities: {name: string, id: number}[] = [
     {
         name: 'Sao Paolo',
         id: 3448439
