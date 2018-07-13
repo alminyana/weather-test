d5536049b570d742868c5572d976642a2b21b0ed
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-13 20:50:30 +0200

    added Observable.interval

--- src/app/cities/cities-list/cities-list.component.ts
+++ src/app/cities/cities-list/cities-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { City } from "../model/city.model";
 import { CitiesService } from "../service/cities.service";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cities-list',
@@ -9,27 +10,44 @@ import { CitiesService } from "../service/cities.service";
 })
 export class CitiesListComponent implements OnInit {
 
+  interval: number = 1000 * 10;
   loaded: boolean = false;
   allCities: City[];
+  countDown: number = this.interval;
   @Output() currentCity: City;
 
   constructor(private citySrv: CitiesService) { }
 
   ngOnInit() {
     this.getCities();
+    //this.showCounter();
   }
 
   getCities(): void {
-    this.citySrv.getAllDesiredCities()
-      .subscribe(
+      const getCitiesInterval = Observable
+        .interval(this.interval)
+        .timeInterval()
+        .do(()=> { this.loaded = false;})
+        .flatMap(() => this.citySrv.getAllDesiredCities());
+
+      getCitiesInterval.subscribe(
         (citiesList: City[]) => {
-          this.allCities = citiesList['list'];
-          this.loaded = true;
-        }
+              this.allCities = citiesList['list'];
+              this.loaded = true;
+          }
       );
   }
 
-  onRefreshData() {
+  showCounter() {
+    const counter: Observable<number> = Observable.interval(1000);
+    counter.subscribe(
+      (number: number) => {
+        this.countDown -= number;
+      }
+    );
+  }
+
+  onRefreshData():void {
     this.loaded = false;
     this.getCities();
   }
