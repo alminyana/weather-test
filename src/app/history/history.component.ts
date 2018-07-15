08f29ee1282ec6b17d8e5cbe1d539225723d760c
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-15 13:07:16 +0200

    page layout improvements

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output } from '@angular/core';
+
+import { AppConfig } from '../cities/app-config';
 import { LocalStorageService } from "../cities/service/localStoreage-service";
 import { City } from "../cities/model/city.model";
 
@@ -10,17 +12,18 @@ import { City } from "../cities/model/city.model";
 export class HistoryComponent implements OnInit {
 
   history: City[];
-  allLima: City[];
+  histLima: City[];
   @Output() one: City;
   constructor(private localSrv: LocalStorageService) { }
 
   ngOnInit() {
     this.history = this.localSrv.getData();
+    this.histLima = this.filterByCity(this.history, AppConfig.cities[2].name);
   }
 
-  filterByCity(list: City[]) {
-    this.allLima = this.history.filter(
-      city => city['name'] ==='Lima'
+  filterByCity(list: City[], cityName: string): City[] {
+    return this.history.filter(
+      city => city['name'] === cityName
     );
   }
 
